ad9159f2f78ba4ee53ead32befbfe4f6957c0e14
pierre <pnavarin@gmail.com>
2021-08-26 17:33:30 -0400

    Finished print grid

--- board.rb
+++ board.rb
@@ -2,12 +2,21 @@ class Board
     attr_reader :grid
 
     def initialize(size = 9)
-        @grid = Array.new(size) {Array.new(size, '[]')}
+        @grid = Array.new(size) {Array.new(size, '[ ]')}
 
     end
 
     def print_grid
-        p '  ' + (0...grid.length).to_a.join(' ') + '  '
+        puts '   ' + (0...grid.length).to_a.join('  ') + '  '
+        grid.each_with_index do |row, i|
+            print i.to_s + ' '
+            row_string = ''
+            row.each do |ele|
+                row_string << ele
+            end
+            puts row_string
+        end
+        
     end
 
 
